691906d21f0cb780a96e3678130e09c33fa74da2
DivyanshuJhamtani <DivyanshuJhamtani@users.noreply.github.com>
2020-01-25 18:29:58 +0530

    some change

---
+++ application/views/admin/sidebar/my_tournaments.php
@@ -0,0 +1,166 @@
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>My Tournaments</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">My Tournaments</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header headerbanner">
+                <h3 class="card-title header-name">Tournament Name</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus cardhead-button"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times cardhead-button"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <a href="#">
+				<div>
+				<img src="<?php echo base_url();?>/assets/admintheme/dist/img/cricktour banner 3.jpg" class="banner-tour">
+				</div>
+				
+				</a>
+				
+              </div>
+              <!-- /.card-body -->
+			  
+              <div class="card-footer foot-banner">
+                <div class="col-md-10 footer-tour footer-text" >12 Jan,2020 to 18 Jun, 2020<br>
+				Jaipur</div>
+				<div class="col-md-2 footer-tours">
+				<button type="button" class="tour-follow" onclick="dj()">
+                  <script>
+				  function dj(){
+				  alert("test");
+				  }
+				  </script>
+				  Follow
+                </button>
+				</div>
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+			
+			
+			
+			
+						<!-- Default box -->
+
+            <div class="card">
+              <div class="card-header headerbanner">
+                <h3 class="card-title header-name">Tournament Name</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus cardhead-button"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times cardhead-button"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <a href="#">
+				<div>
+				<img src="<?php echo base_url();?>/assets/admintheme/dist/img/cricktour banner 1.jpg" class="banner-tour">
+				</div>
+				
+				</a>
+				
+              </div>
+              <!-- /.card-body -->
+			  
+              <div class="card-footer foot-banner">
+                <div class="col-md-10 footer-tour" >22 Aug,2020 to 29 Sep, 2020<br>
+				Delhi</div>
+				<div class="col-md-2 footer-tours">
+				<button type="button" class="tour-follow" onclick="dj()">
+                  <script>
+				  function dj(){
+				  alert("test");
+				  }
+				  </script>
+				  Follow
+                </button>
+				</div>
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+			
+			
+			
+			<!-- Default box -->
+            <div class="card">
+              <div class="card-header headerbanner">
+                <h1 class="card-title header-name">Tournament Name</h1>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus cardhead-button"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times cardhead-button"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <a href="#">
+				<div>
+				<img src="<?php echo base_url();?>/assets/admintheme/dist/img/cricktour banner 2.jpg" class="banner-tour">
+				</div>
+				
+				</a>
+				
+              </div>
+              <!-- /.card-body -->
+			  
+              <div class="card-footer foot-banner">
+                <div class="col-md-10 footer-tour" >28 Mar,2020 to 18 May, 2020<br>
+				Ajmer</div>
+				<div class="col-md-2 footer-tours">
+				<button type="button" class="tour-follow" onclick="dj()">
+                  <script>
+				  function dj(){
+				  alert("test");
+				  }
+				  </script>
+				  Follow
+                </button>
+				</div>
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+			
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+	</div><!-- /.content-wrapper -->
+	
+	  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
